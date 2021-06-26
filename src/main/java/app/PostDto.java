a31f2bfe0bf459d7caa01666327d3a049ed38e65
matsarat <mat.saratowicz@gmail.com>
2021-06-26 12:31:25 +0200

    Create PostDto class

---
+++ src/main/java/app/PostDto.java
@@ -0,0 +1,61 @@
+package app;
+
+public class PostDto {
+
+    private long userId;
+    private long id;
+    private String title;
+    private String body;
+
+    public PostDto(long userId, long id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public PostDto() {
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "PostDto{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
